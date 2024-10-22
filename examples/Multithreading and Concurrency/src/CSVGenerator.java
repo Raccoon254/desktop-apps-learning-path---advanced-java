24003902095e7575c822c39672355d3014a34319
Raccoon254 <tomsteve187@gmail.com>
2024-10-22 21:54:31 +0300

    Created csv generator

---
+++ examples/Multithreading and Concurrency/src/CSVGenerator.java
@@ -0,0 +1,40 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class CSVGenerator {
+    public static void main(String[] args) {
+        try {
+            PrintWriter writer = 
+                    new PrintWriter(new FileWriter("phone_numbers.csv"));
+            
+            Random random = new Random();
+            String[] firstNames = {"John", "Jane", "Alice", "Bob", "Charlie", "Steve"};
+            String[] lastNames = {"Doe", "Smith", "Johnson", "Brown", "Lee", "Davis"};
+
+            for (int i = 1; i <= 100_000 ; i++) {
+                String name = firstNames[random.nextInt(firstNames.length)] + " " + lastNames[random.nextInt(lastNames.length)];
+                String phoneNumber;
+                
+                //chance
+                if (random.nextDouble() > 0.8) {
+                    phoneNumber = String.format("%03d-%03d-%04d",
+                            random.nextInt(1000),
+                            random.nextInt(1000),
+                            random.nextInt(10000));
+                }else {
+                    phoneNumber = String.format("%d-%d-%d",
+                            random.nextInt(10),
+                            random.nextInt(10),
+                            random.nextInt(10000));
+                }
+                
+                writer.printf("%d,%s,%s%n", i, name, phoneNumber);
+            }
+
+            System.out.println("Generated users");
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ examples/Multithreading and Concurrency/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    
+    private volatile int age = 10;
+    
+    
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        MyThread yourThread = new MyThread();
+        thread.start();
+        yourThread.start();
+        
+        Thread threadTest = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread :" + Thread.currentThread().getId() + " Current iteration " + i);
+                try {
+                    Thread.sleep(1000);
+                }catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+        
+        threadTest.start();
+    }
+}
