ade7bc5138a36303e6584d6d5e33b05abaad0a1a
Raccoon254 <tomsteve187@gmail.com>
2024-10-22 21:35:54 +0300

    Complete threads example -- implementation

---
+++ examples/Multithreading and Concurrency/src/MyThread.java
@@ -0,0 +1,13 @@
+public class MyThread extends Thread{
+    @Override
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread :" + Thread.currentThread().getId() + " Current iteration " + i);
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
